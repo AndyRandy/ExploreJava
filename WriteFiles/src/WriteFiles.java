53c1f8cd087c14e0720e1c659ced4bf49d691028
AndyRandy <a.jadrin@campus.lmu.de>
2015-11-15 12:42:56 +0100

    get user home dir irrespective of the underlying os

---
+++ WriteFiles/src/WriteFiles.java
@@ -0,0 +1,16 @@
+
+
+/**
+ * Created by Deus on 15.11.2015.
+ */
+public class WriteFiles {
+
+    public static void main(String[] args) {
+
+        //get user home dir irrespective of the underlying OS
+        String nameHomeDir = System.getProperty("user.home");
+        System.out.println("Home dir: "+ nameHomeDir);
+
+        //return path of a file
+    }
+}
